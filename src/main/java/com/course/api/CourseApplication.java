61b457df4baa7925134f3a8b9ed36c4bbc4499af
araba <araba.mohamed@gmail.com>
2024-07-24 19:55:31 +0200

    modifying application-dev.yml, and testing mutable and immutable collection in java

--- src/main/java/com/course/api/CourseApplication.java
+++ src/main/java/com/course/api/CourseApplication.java
@@ -8,6 +8,18 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+
+		// // test de la méthode Collections.singletonMap()
+		// // Création d'une carte immuable avec une seule entrée
+		// Map<String, String> singleEntryMap = Collections.singletonMap("courseName", "Java Programming");
+        // // Création d'une nouvelle carte mutable basée sur la carte immuable
+        // Map<String, String> mutableMap = new HashMap<>(singleEntryMap);
+        // // Modification de la nouvelle carte mutable
+        // mutableMap.put("courseLevel", "Intermediate");
+        // // Affichage de la carte modifiée
+        // System.out.println("Mutable Map: " + mutableMap);
+
+
 		// var ctx = SpringApplication.run(CourseApplication.class, args);
 		// var app = new SpringApplication(CourseApplication.class);
 		// app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
---
+++ src/main/java/com/course/api/models/Student.java
@@ -0,0 +1,56 @@
+package com.course.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "T_Student")
+public class Student {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name = "c_firstname", length=20)
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private int age;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
